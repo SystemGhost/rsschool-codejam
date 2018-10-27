88765fca5837a6292d2b3a419606bdd37be7d9fc
SystemGhost <jackie07364538@gmail.com>
2018-10-28 01:25:46 +0300

    feat: add make function

---
+++ make.js
@@ -0,0 +1,13 @@
+function make(...arg) {
+  const arr = [];
+  arr.push(...arg);
+  function f(...b) {
+    arr.push(...b);
+    if (typeof arr[arr.length - 1] === 'function') {
+      const func = arr.pop();
+      return arr.reduce(func, 0);
+    }
+    return f;
+  }
+  return f;
+}
--- sumOfOther.js
+++ sumOfOther.js
@@ -1,10 +1,8 @@
 function sumOfOther(arg) {
-  let arr = [];
-  let result = arg.reduce(function(previousValue, currentValue) {
-    return previousValue + currentValue;
-  }, 0);
+  const arr = [];
+  const result = arg.reduce((previousValue, currentValue) => previousValue + currentValue, 0);
 
-  for(let i = 0; i < arg.length; i++) {
+  for (let i = 0; i < arg.length; i += 1) {
     arr[i] = result - arg[i];
   }
   return arr;
